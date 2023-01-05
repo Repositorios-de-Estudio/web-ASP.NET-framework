f7ae72d569668f4dad2f7f0bf678307fe43092ee
SergioPerez <dark07perez@gmail.com>
2023-01-04 22:38:01 -0500

    Update SendMailController.cs

--- RegistroEmpleados/RegistroEmpleados/Controllers/SendMailController.cs
+++ RegistroEmpleados/RegistroEmpleados/Controllers/SendMailController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
@@ -17,29 +18,25 @@ namespace RegistroEmpleados.Controllers
         }
 
         [HttpPost]
-        public ViewResult Index(MailModel _objModelMail)
+        public ViewResult Index(MailModel mailWEB)
         {
             if (ModelState.IsValid)
             {
-                MailMessage mail = new MailMessage();
+                MailAddress desde = new MailAddress("FromAddress");
+                MailAddress hacia = new MailAddress("ToAddress");
+                MailMessage mensaje = new MailMessage(desde, hacia);
+                mensaje.Subject = "testmail";
+                mensaje.Body = "testmail";
+                mensaje.IsBodyHtml = false;
+                SmtpClient client = new SmtpClient();
+                client.Port = ;
+                client.Host = "exchange server host address";
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("username", "password", "domain");
+                client.Send(mensaje);
 
-                mail.To.Add(_objModelMail.To);
-                mail.From = new MailAddress(_objModelMail.From);
-                mail.Subject = _objModelMail.Subject;
-                string Body = _objModelMail.Body;
-                mail.Body = Body;
-                mail.IsBodyHtml = true;
-
-                SmtpClient smtp = new SmtpClient();
-
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new System.Net.NetworkCredential("username", "password"); // Enter seders User name and password  
-                smtp.EnableSsl = true;
-                smtp.Send(mail);
-
-                return View("Index", _objModelMail);
+                return View("Index", mailWEB);
             }
             else
             {
